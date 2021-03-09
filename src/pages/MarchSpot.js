e37b0e579a37327094107d181b96a39c64425ab7
kimmitutoring <kimmi@tutoring.co.kr>
2021-03-09 15:45:01 +0900

    classNames 테스트

--- src/pages/MarchSpot.js
+++ src/pages/MarchSpot.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './MarchSpot.scss';
+import classNames from "classnames/bind";
+import style from "./MarchSpot.scss";
 
-function MarchSpot() {
+const cx = classNames.bind(style);
+
+const MarchSpot = () => {
 	return (
-		<div classNames="march-spot">
-			<h1 classNames="spot__title">3월 아주땡큐 1+1 이벤트</h1>
-			<p classNames="spot__desc">Thank you very march 1+1 event (3,000,000 DOWNLOADED)</p>
-			<span classNames="spot__period">03.11 to 03.21</span>
+		<div className={cx('march-spot')}>
+			<h1 className={cx('spot__title')}>3월 아주땡큐 1+1 이벤트</h1>
+			<p className={cx('spot__desc')}>Thank you very march 1+1 event (3,000,000 DOWNLOADED)</p>
+			<span className={cx('spot__period')}>03.11 to 03.21</span>
 		</div>
 	)
 }
