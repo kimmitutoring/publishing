375cc9a8f61212b12f21edd7c866019c1ef7d08c
kimmitutoring <kimmi@tutoring.co.kr>
2021-03-09 15:14:30 +0900

    node-sass 추가

---
+++ src/components/MarchSpot.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MarchSpot() {
+	return (
+		<div class="march-spot">
+			<h1 class="spot__title">3월 아주땡큐 1+1 이벤트</h1>
+			<p class="spot__desc">Thank you very march 1+1 event (3,000,000 DOWNLOADED)</p>
+			<span class="spot__period">03.11 to 03.21</span>
+		</div>
+	)
+}
+
+export default MarchSpot;
